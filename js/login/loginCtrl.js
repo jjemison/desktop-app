d97e4ea48b5721ecae0a96a8110fb57397e30254
Jordan <jordanjemison@gmail.com>
2015-04-27 16:25:28 -0600

    add Add New Website functionailty

--- js/login/loginCtrl.js
+++ js/login/loginCtrl.js
@@ -2,6 +2,24 @@ var app = angular.module('desktop');
 
 app.controller('loginCtrl', function($scope){
   
+$scope.contacts = [
+    { name: 'Section Name'},
+  ];
+
+    $scope.myVar = false;
+    $scope.toggle = function() {
+        $scope.myVar = !$scope.myVar;
+    };
+
+    $scope.lists = [];
+
+    $scope.newListItem = function(){
+
+      $scope.lists.push({name:$scope.name, url:$scope.url});
+      $scope.name = '';
+      $scope.url = '';
+
+    };
 
 
 })
